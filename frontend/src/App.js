7dfeeab43190ba3a876cfb033521345f806abb25
anumshka <anushka.mpathak17@gmail.com>
2022-06-21 03:13:47 +0530

    use of react routers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,25 @@ import Footer from './components/Footer'
 import Header from './components/Header'
 import { Container } from 'react-bootstrap'
 import HomeScreen from './screens/HomeScreen'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
     <>
-      <Header></Header>
+      <Router>
+        <Header></Header>
 
-      <main className='py-3'>
-        <Container>
-          <h1>What you reading?</h1>
-          <HomeScreen></HomeScreen>
-        </Container>
-      </main>
+        <main className='py-3'>
+          <Container>
+            <h1>What you reading?</h1>
+            <Route path='/' component={HomeScreen} exact />
+            <Route path='/product/:id' component={ProductScreen} />
+          </Container>
+        </main>
 
-      <Footer></Footer>
+        <Footer></Footer>
+      </Router>
     </>
   )
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,27 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 const Header = () => {
   return (
     <Navbar bg='light' expand='lg'>
       <Container>
-        <Navbar.Brand href='/'>Book-Fair</Navbar.Brand>
+        <LinkContainer to='/'>
+          <Navbar.Brand>Book-Fair</Navbar.Brand>
+        </LinkContainer>
+
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='me-auto'>
-            <Nav.Link href='/cart' style={{ color: 'black' }}>
-              <i className='fas fa-shopping-cart'></i>Cart
-            </Nav.Link>
-            <Nav.Link href='/sign-in' style={{ color: 'black' }}>
-              <i className='fas fa-user'></i>SIGN-IN
-            </Nav.Link>
+            <LinkContainer to='/cart' style={{ color: 'black' }}>
+              <Nav.Link>
+                <i className='fas fa-shopping-cart'></i>Cart
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to='/sign-in' style={{ color: 'black' }}>
+              <Nav.Link>
+                <i className='fas fa-user'></i>SIGN-IN
+              </Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
